54e31fd5157b8cc7d68d6e1fbfdf43464c4368b1
ranjith190291 <ranjithkumar1902@gmail.com>
2026-07-08 11:15:27 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,26 @@
+/* 1. String Datatype */
+
+let userName = "ranj1902"
+console.log("1. Username is" + " "+ userName + " " + "and the DataType is" + " " + typeof(userName));
+
+/* 2. String Datatype */
+
+let organization = "Wipro Limited"
+console.log("2. Organization is" + " " + organization + " " + "and the DataType is" + " " + typeof(organization))
+
+/* 3. Number Datatype */
+
+let contactNumber = 9067890143
+console.log("3. Contact Number is" + " " + contactNumber + " " + "and the DataType is" + " " + typeof(contactNumber));
+
+/* 4. Boolean Datatype */
+
+let knowsAutomation = false
+console.log("4. Knows Automation is" + " " + knowsAutomation + " " + "and the DataType is" + " " + typeof(knowsAutomation));
+
+/* 5. Undefined Datatype */
+
+let usesPlaywright
+console.log("5. Uses Playwright is" + " " + usesPlaywright + " " + "and the DataType is" + " " + typeof(usesPlaywright));
+
+
